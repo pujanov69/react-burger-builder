7538726d98c87fafc9093758d27dd8c89c1f1c0f
pujanov69 <pujanov69@gmail.com>
2021-01-26 13:33:38 +0545

    orders side effects into sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,6 +1,7 @@
 import { takeEvery } from "redux-saga/effects";
 
 import * as actionTypes from "../actions/actionTypes";
+import { fetchOrdersSaga, purchaseBurgerSaga } from "./order";
 import {
   logoutSaga,
   checkAuthTimeoutSaga,
@@ -19,4 +20,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
   yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
